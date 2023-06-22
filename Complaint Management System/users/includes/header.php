9f50b6901b5e3cceaf2b7df890556ec99dfaa806
alankritamingwal <mingwal.alankrita@gmail.com>
2023-06-22 14:45:29 +0530

    file

---
+++ Complaint Management System/users/includes/header.php
@@ -0,0 +1,22 @@
+ <header class="header black-bg">
+              <div class="sidebar-toggle-box">
+                  <div class="fa fa-bars tooltips" data-placement="right" data-original-title="Toggle Navigation"></div>
+              </div>
+            <!--logo start-->
+            <a href="#" class="logo">
+                <img src="assets/img/logo2.png" style="
+                width: 40px;
+                height: 40px;
+                margin: -7px 5px 0 0;
+                -webkit-border-radius: 50%;
+                -moz-border-radius: 50%;
+                border-radius: 50%;"/>
+                <b>Complaint Management System</b>
+            </a>
+                
+            <div class="top-menu">
+            	<ul class="nav pull-right top-menu">
+                    <li><a class="logout" href="logout.php">Logout</a></li>
+            	</ul>
+            </div>
+        </header>
